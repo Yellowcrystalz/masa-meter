b15730aec3eff52353d6b2ac51c3675522a0e774
yellowcrystalz <jaykaycrystal0914@gmail.com>
2025-08-26 21:02:09 -0500

    changed startup.sh to bash

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
@@ -17,17 +17,6 @@ cleanup() {
     exit 0
 }
 
-trap 'cleanup; exit 0' EXIT INT TERM
-
-while true; do
-    if ! kill -0 $BOT_PID 2>/dev/null; then
-        break
-    fi
-    if ! kill -0 $WEBAPP_PID 2>/dev/null; then
-        break 
-    fi
-    sleep 1
-done
-
-cleanup
+trap cleanup EXIT INT TERM
+wait -n $BOT_PID $WEBAPP_PID
 echo "Exit Successful"
